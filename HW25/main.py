0f9fa4ff3e58955357455740580e8889069eb164
serhiihero <serhiiherasymovych+93@gmail.com>
2023-05-21 22:13:44 +0300

    task HW25 done

---
+++ HW25/main.py
@@ -0,0 +1,4 @@
+# Create TeamCity Server (in docker is the best way but you can try to configure it on the local machine)
+# Create Teamcity agent (in docker is the best way but you can try to configure it on local machine)
+# Run your first build job from your repository on git.
+# Attach screenshots of the result.
