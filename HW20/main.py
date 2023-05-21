62c92c6f3aee05a110052b43dad456ffa8491b1b
serhiihero <serhiiherasymovych+93@gmail.com>
2023-05-21 22:03:11 +0300

    task HW20 done

---
+++ HW20/main.py
@@ -0,0 +1,4 @@
+# Add an allure report to your framework.
+# Mark steps in your PageObjects and API calls.
+# Make a screenshot on fail for allure.
+# Attach screenshots of your allure report as this HW
